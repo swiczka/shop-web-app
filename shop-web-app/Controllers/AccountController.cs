a1287f3a5785261ee52bd3edaab89813ec3f4744
Damian Świeca <damianswieca04@gmail.com>
2024-11-11 19:21:50 +0100

    working login

---
+++ shop-web-app/Controllers/AccountController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using shop_web_app.Data;
+using shop_web_app.Models;
+using shop_web_app.ViewModels;
+
+namespace shop_web_app.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly ApplicationDbContext _context;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ApplicationDbContext context) 
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+
+        public IActionResult Login()
+        {
+            //response is there for keeping form data from vanishing when reloading page
+            var response = new LoginViewModel();
+            return View(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(loginVM);
+            }
+
+            var user = await _userManager.FindByEmailAsync(loginVM.Email);
+
+            if(user != null)
+            {
+                var passwordCheck = await _userManager.CheckPasswordAsync(user, loginVM.Password);
+                if(passwordCheck)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+                    if(result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                TempData["Error"] = "Niepoprawne hasło, spróbuj ponownie.";
+                return View(loginVM);
+            }
+            TempData["Error"] = "Nie znaleziono konta o takim adresie e-mail.";
+            return View(loginVM);
+        }
+
+        public IActionResult Register()
+        {
+
+        }
+    }
+}
---
+++ shop-web-app/ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace shop_web_app.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Display(Name = "Adres e-mail")]
+        [Required(ErrorMessage = "Adres e-mail jest wymagany")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Hasło jest wymagane")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Hasło")]
+        public string Password { get; set; }
+    }
+}
