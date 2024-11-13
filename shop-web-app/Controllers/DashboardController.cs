b65bbaa2115fe87c188319c9f0422dbafaec3c59
Damian Świeca <damianswieca04@gmail.com>
2024-11-13 13:51:56 +0100

    new navbar & footer

---
+++ shop-web-app/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace shop_web_app.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index() 
+        { 
+            return View();
+        }
+    }
+}
