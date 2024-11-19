6d655030e94353af75c2a212224e0c28f743a7a1
Damian Świeca <damianswieca04@gmail.com>
2024-11-19 15:28:54 +0100

    started order development

---
+++ shop-web-app/Controllers/OrderController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using shop_web_app.Interfaces;
+using shop_web_app.Models;
+using shop_web_app.ViewModels;
+
+namespace shop_web_app.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly UserManager<AppUser> _userManager;
+
+        public OrderController(IUserRepository userRepository, UserManager<AppUser> userManager) 
+        {
+            _userRepository = userRepository;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            List<CartItem> cartItems = await _userRepository.GetCartItems(_userManager.GetUserId(User));
+            AppUser user = await _userManager.GetUserAsync(User);
+
+            CreateOrderViewModel vm = new CreateOrderViewModel()
+            {
+                CartItems = cartItems,
+                AppUser = user
+            };
+            return View(vm);
+        }
+    }
+}
---
+++ shop-web-app/ViewModels/CreateOrderViewModel.cs
@@ -0,0 +1,10 @@
+﻿using shop_web_app.Models;
+
+namespace shop_web_app.ViewModels
+{
+    public class CreateOrderViewModel
+    {
+        public List<CartItem> CartItems { get; set; }
+        public AppUser AppUser { get; set; }
+    }
+}
